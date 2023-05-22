612a52cd70e8c4fe0eb520e658c3cfd0f061d319
Vuyisa Baza <vuyisa.baza@entelect.co.za>
2023-05-22 16:02:28 +0200

    refactored codebase to fetch using React router

--- src/posts/PostsPage.tsx
+++ src/posts/PostsPage.tsx
@@ -1,43 +1,47 @@
-import React, { useEffect, useState } from "react";
-import { getPosts } from "./getPosts";
+import React, { useEffect, useState, Suspense } from "react";
+import { assertIsPosts } from "./getPosts";
 import { PostData, NewPostData } from "./types";
 import { PostsList } from "./PostsList";
 import { savePost } from "./savePost";
 import { NewPostForm } from "./NewPostForm";
+import { useLoaderData, Await } from "react-router-dom";
 
 export function PostsPage() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [posts, setPosts] = useState<PostData[]>([]);
-
-  useEffect(() => {
-    let cancel = false;
-
-    getPosts().then((data) => {
-      console.log(data);
-      if (!cancel) {
-        setPosts(data);
-        setIsLoading(false);
-      }
-    });
-    return () => {
-      cancel = true;
-    };
-  }, []);
-
+  const data = useLoaderData();
+  assertIsData(data);
   async function handleSave(newPostData: NewPostData) {
-    const newPost = await savePost(newPostData);
-    setPosts([newPost, ...posts]);
-  }
-
-  if (isLoading) {
-    return <div className="w-96 mx-auto mt-6">Loading ...</div>;
+    await savePost(newPostData);
   }
 
   return (
-    <div className="w-96 mx-auto mt-6">
-      <h2 className="text-xl text-slate-900 font-bold">Posts</h2>
-      <NewPostForm onSave={handleSave} />
-      <PostsList posts={posts} />
-    </div>
+    <Suspense fallback={<div>Fetching...</div>}>
+      <Await resolve={data.posts}>
+        {(posts) => {
+          assertIsPosts(posts);
+          return (
+            <div className="w-96 mx-auto mt-6">
+              <h2 className="text-xl text-slate-900 font-bold">Posts</h2>
+              <NewPostForm onSave={handleSave} />
+              <PostsList posts={posts} />
+            </div>
+          );
+        }}
+      </Await>
+    </Suspense>
   );
+}
+
+type Data = {
+  posts: PostData[];
+};
+export function assertIsData(data: unknown): asserts data is Data {
+  if (typeof data !== "object") {
+    throw new Error("Data isn't an object");
+  }
+  if (data === null) {
+    throw new Error("Data is null");
+  }
+  if (!("posts" in data)) {
+    throw new Error("data doesn't contain posts");
+  }
 }
